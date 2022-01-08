f052e119e349aef06b12b945b23203209fee09a8
andreea andra <andrea_bedr@yahoo.com>
2022-01-08 22:01:11 +0200

    aproape gata

--- Pages/Reservations/Index.cshtml.cs
+++ Pages/Reservations/Index.cshtml.cs
@@ -21,9 +21,20 @@ namespace BookingAppWEB.Pages.Reservations
 
         public IList<Reservation> Reservation { get;set; }
 
-        public async Task OnGetAsync()
+        public ReservationData ReservationD { get; set; }
+        public int ReservationID { get; set; }
+        public int RoomID { get; set; }
+        public async Task OnGetAsync(int? id, int? roomID)
         {
-            Reservation = await _context.Reservation.Include(b => b.User).ToListAsync();
+            ReservationD = new ReservationData();
+
+            ReservationD.Reservations = await _context.Reservation.Include(b => b.User).Include(b => b.ReservationRooms).ThenInclude(b => b.Room).AsNoTracking().ToListAsync();
+            if (id != null)
+            {
+                ReservationID = id.Value;
+                Reservation reservation = ReservationD.Reservations.Where(i => i.ID == id.Value).Single();
+                ReservationD.Rooms = reservation.ReservationRooms.Select(s => s.Room);
+            }
         }
     }
 }
